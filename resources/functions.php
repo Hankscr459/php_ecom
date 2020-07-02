bad9b3be8d31f594dda20e6d574c59e01ebe8e3c
xrudoga <defcw58@gmail.com>
2020-07-02 15:28:47 +0800

    Product function

--- resources/functions.php
+++ resources/functions.php
@@ -1,4 +1,7 @@
 <?php
+
+    // helper functions
+
     function redirect($location) {
         header("Location: $location ");
     }
@@ -23,4 +26,100 @@
     function fetch_array($result) {
         return mysqli_fetch_array($result);
     }
+
+/** FRONT END FUNCTIONS */
+
+    // get products
+
+    function get_products() {
+        $query = query(" SELECT * FROM products");
+        confirm($query);
+        while($row = fetch_array($query)) {
+            $product = <<<DELIMETER
+                <div class="col-sm-4 col-lg-4 col-md-4">
+                    <div class="thumbnail">
+                        <a target="_blank" href="item.php?id={$row['product_id']}"><img src="{$row['product_image']}" alt=""></a>
+                        <div class="caption">
+                            <h4 class="pull-right">&#36;{$row['product_price']}</h4>
+                            <h4><a href="item.php?id={$row['product_id']}">{$row['product_title']}</a>
+                            </h4>
+                            <p>This is a short description. <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a></p>
+                            <a class="btn btn-primary" target="_blank" href="item.php?id={$row['product_id']}">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+            DELIMETER;
+
+            echo $product;
+        }
+    }
+
+    function get_categories() {
+        
+        $query = query("SELECT * FROM categories");
+        confirm($query);
+
+        while($row = fetch_array($query)) {
+            $category_links = <<<DELIMETER
+                <a href='category.php?id={$row['cat_id']}' class='list-group-item'>{$row['cat_title']}</a>
+            DELIMETER;
+            echo $category_links;
+        }
+    }
+
+    function get_products_in_cat_page() {
+        $query = query(" SELECT * FROM products WHERE product_category_id = " . escape_string($_GET['id']). " ");
+        confirm($query);
+        while($row = fetch_array($query)) {
+            $product = <<<DELIMETER
+                
+                <div class="col-md-3 col-sm-6 hero-feature">
+                    <div class="thumbnail">
+                        <img src="{$row['product_image']}" alt="">
+                        <div class="caption">
+                            <h3>{$row['product_title']}</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                            <p>
+                                <a href="#" class="btn btn-primary">Buy Now!</a> <a href="item.php?id={$row['product_id']}" class="btn btn-default">More Info</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            DELIMETER;
+
+            echo $product;
+        }
+    }
+
+    function get_products_in_shop_page() {
+        $query = query(" SELECT * FROM products");
+        confirm($query);
+        while($row = fetch_array($query)) {
+            $product = <<<DELIMETER
+                
+                <div class="col-md-3 col-sm-6 hero-feature">
+                    <div class="thumbnail">
+                        <img src="{$row['product_image']}" alt="">
+                        <div class="caption">
+                            <h3>{$row['product_title']}</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                            <p>
+                                <a href="#" class="btn btn-primary">Buy Now!</a> <a href="item.php?id={$row['product_id']}" class="btn btn-default">More Info</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            DELIMETER;
+
+            echo $product;
+        }
+    }
+
+
+
+
+/** FRONT END FUNCTIONS */
+
+
+
 ?>
